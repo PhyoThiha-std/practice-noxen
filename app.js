7fcd17e9b8beb55b7cbee5c5756c4ba10b41f756
Phyo <meowmeowphyo27@gmail.com>
2022-09-10 17:49:36 +0630

    my first

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+
+
+    // Footer Section
+        const getyear = $("#getyear")
+        const getfullyear = new Date().getUTCFullYear()
+        getyear.text(getfullyear)
+})
